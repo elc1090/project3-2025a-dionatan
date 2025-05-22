0865f85e7d6c2cdee387d6d11b544c390373a179
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2025-05-22 09:38:12 -0300

    🔐 refactor : login Auth

--- src/components/auth-page/index.tsx
+++ src/components/auth-page/index.tsx
@@ -8,8 +8,8 @@ export const AuthPage = (props: AuthPageProps) => {
       {...props}
       formProps={{
         defaultValues: {
-          email: "dionatan@refine.supabase",
-          password: "dev-web",
+          email: "info@refine.dev",
+          password: "refine-'supabase'",
         },
       }}
     />
